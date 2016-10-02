ed0eed1ceb877e8f58aa19be99aa1b0ad7c5f0d1
Daniel Cheung <daniel.2345@hotmail.com>
2016-10-02 11:29:47 +0800

    Basic Interface

---
+++ _includes/generals.php
@@ -0,0 +1,43 @@
+<?php
+$path = $_SERVER["DOCUMENT_ROOT"];
+$app_url = "http://".$_SERVER['HTTP_HOST']."/".
+    trim(str_replace(str_replace('\\', "/", $path), "", str_replace('\\', "/", dirname(__DIR__))), "/")."/";
+
+
+
+function print_header($title) {
+    global $app_url;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?php echo $title;?></title>
+    <link href="<?php echo $app_url;?>css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo $app_url;?>css/font-awesome.min.css" rel="stylesheet">
+    <script src="<?php echo $app_url;?>js/jquery.min.js"></script>
+    <script src="<?php echo $app_url;?>js/bootstrap.min.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    Meet When?
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Create an event</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+<?php
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include_once("_includes/generals.php");
+$title = "Meet When?";
+print_header("Meet When?");
+?>
+
+<?php
+include_once($app_url."_includes/footer.php");
+?>
